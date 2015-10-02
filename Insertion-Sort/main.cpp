c860adfa9e99ab376089e564275c54422e16401d
Desislava Stoyanova <dstoyanova@desislavas-macbook-air.local>
2015-10-02 16:03:07 +0200

    Initial Commit

---
+++ Insertion-Sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Insertion-Sort
+//
+//  Created by Desislava Stoyanova on 10/2/15.
+//  Copyright © 2015 Desislava Stoyanova. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
